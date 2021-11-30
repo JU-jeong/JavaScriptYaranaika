c81b40acfe43ac53fcb4f97e3b7e2d7242724951
wnwjdtjr77 <wnwjdtjr77@naver.com>
2021-11-30 13:28:22 +0900

    JS1

---
+++ script1.js
@@ -0,0 +1,10 @@
+window.alert('hello!')
+//주석
+/*window.console = console 둘 다 사용가능
+자바스크립트에서는 대소문자 구분 철저히
+browser객체
+window.console*/ 
+window.console.log("주정석") //콘솔화면에 기록을 남기겠다
+console.log('고양이')
+console.log("하나 더")
+console.clear() //?
